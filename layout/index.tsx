03765ab78d416896a6331c728b1ecf54e1357ad7
mos <ysg01129@naver.com>
2022-01-01 15:58:32 +0900

    fix(#14): scroll issue

--- layout/index.tsx
+++ layout/index.tsx
@@ -1,7 +1,12 @@
-import React from "react";
+import React, {useEffect} from "react";
 import {Background} from "@/components/common/Background";
 
 export default function Layout({children}: { children: React.ReactNode }) {
+    useEffect(() => {
+        setTimeout(function() {
+            window.scrollTo(0, 1);
+        }, 0);
+    }, [])
     return (
         <Background>
             <main>
