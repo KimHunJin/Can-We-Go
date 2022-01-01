6a33633fc7c3ecf637dd4f6521e27f74f1907dd9
mos <ysg01129@naver.com>
2022-01-01 16:18:25 +0900

    fix(#14): scroll issue test

--- layout/index.tsx
+++ layout/index.tsx
@@ -3,6 +3,15 @@ import {Background} from "@/components/common/Background";
 
 export default function Layout({children}: { children: React.ReactNode }) {
     useEffect(() => {
+        let vh = window.innerHeight * 0.01;
+        document.documentElement.style.setProperty("--vh", `${vh}px`);
+
+        window.addEventListener("resize", () => {
+            console.log("resize");
+            let vh = window.innerHeight * 0.01;
+            document.documentElement.style.setProperty("--vh", `${vh}px`);
+        });
+
         setTimeout(function() {
             window.scrollTo(0, 1);
         }, 0);
